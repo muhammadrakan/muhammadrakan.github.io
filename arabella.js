e3e02d41d400914c02c296c3a63fcc4fa136ff38
muhammadrakan <muhammadrakan204@gmail.com>
2022-11-07 23:27:35 +0700

    menambahkan efek parallax pada bagian about dan description dan menambahkan jQuery

--- arabella.js
+++ arabella.js
@@ -1,1 +1,14 @@
+$(window).scroll(function() {
+   var wScroll = $(this).scrollTop(); 
+   
+   
+   if( wScroll > $('.description').offset().top - 450) {
+        $('.description h3').addClass('muncul');
+        $('.description h1').addClass('muncul');
+        $('.description .teks').addClass('muncul');
+   }
 
+   if( wScroll > $('.rooms').offset().top - 340 ) {
+      $('.rooms .title-rooms').addClass('muncul');
+   }
+});
